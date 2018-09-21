836791ca028d4eeb1f1e00d45d0116aa268c6a21
Kirsten Thobe <kirsten.thobe@gmail.com>
2018-09-21 15:45:38 +0200

    testing interaction graph

--- script20918.py
+++ script20918.py
@@ -1,12 +1,24 @@
 # PyBoolNet program to create an interaction graph from a boolnet file (e.g. exported from GINsim)
+# To run it, go in pyboolnet conda environment and call it with python
 
 import PyBoolNet
 import FileExchange
 import InteractionGraphs 
 
-primes = FileExchange.bnet2primes("/home/kthobe/mdcstg1/PyBoolNet/TNFLTbRmodel.bnet")
+
+bnet_file = "/home/kthobe/Programs/PyBoolNet_NFkB/TNFLTbRmodel.bnet"
+
+primes = FileExchange.bnet2primes(bnet_file)
 
 igraph = PyBoolNet.InteractionGraphs.primes2igraph(primes)
+igraph.graph["rankdir"] = "TB"
+#InteractionGraphs.add_style_inputs(igraph)
+subgraphs =[(["TNFR","NEMO_IKK","IkBa", "RelA_p50c", "RelA_p50n"],{"label":"Canonical pathway"}),
+	    (["LTbR","TRAF3", "NIK_IKKa", "p100p", "RelB_p52"],{"label":"Noncanonical pathway"})]
+InteractionGraphs.add_style_subgraphs(igraph,subgraphs)
+print(igraph.nodes())
 
-PyBoolNet.InteractionGraphs.igraph2image(igraph,"/home/kthobe/Programs/TNF_LTbR2.pdf")
+
+
+PyBoolNet.InteractionGraphs.igraph2image(igraph,"/home/kthobe/Programs/PyBoolNet_NFkB/TNF_LTbR2.pdf")
 
